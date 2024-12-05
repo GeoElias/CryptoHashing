e8280b0b5c75e187125efbf2d10c43907d0203af
GeoElias <156236706+GeoElias@users.noreply.github.com>
2024-12-04 21:09:35 -0500

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,20 @@
+const demoKeys = [
+    {
+        company: "Company A",
+        publicKey: `-----BEGIN PUBLIC KEY-----
+        ...YourPublicKeyForCompanyA...
+        -----END PUBLIC KEY-----`,
+        privateKey: `-----BEGIN PRIVATE KEY-----
+        ...YourPrivateKeyForCompanyA...
+        -----END PRIVATE KEY-----`,
+    },
+    {
+        company: "Company B",
+        publicKey: `-----BEGIN PUBLIC KEY-----
+        ...YourPublicKeyForCompanyB...
+        -----END PUBLIC KEY-----`,
+        privateKey: `-----BEGIN PRIVATE KEY-----
+        ...YourPrivateKeyForCompanyB...
+        -----END PRIVATE KEY-----`,
+    },
+];
